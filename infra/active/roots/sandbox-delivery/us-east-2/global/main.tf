f5157e2bfd57f28c7ebe7d8371713c64941cec03
hatan4ik <hatan4ik@gmail.com>
2026-09-22 18:45:48 +0300

    fix: permit workload autoscaling target tags

--- infra/active/roots/sandbox-delivery/us-east-2/global/main.tf
+++ infra/active/roots/sandbox-delivery/us-east-2/global/main.tf
@@ -1,5 +1,5 @@
 module "sandbox_delivery_iam" {
-  source = "git::https://github.com/hatan4ik/aws.modules.iam.git?ref=865b97345848df953cb4e7bd4e1d86eb718d5e16" # v0.1.7
+  source = "git::https://github.com/hatan4ik/aws.modules.iam.git?ref=b53a4ec4ace75530d62b39452460c79300c4999e" # v0.1.8
 
   aws_account_id          = var.aws_account_id
   aws_region              = var.aws_region
